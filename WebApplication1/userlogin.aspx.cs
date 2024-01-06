85a44fc7c3eab4c86ce2e02dd85376c2d35ebe7f
EksithMaduranga <eksithmadu9@gmail.com>
2024-01-06 11:24:15 +0530

    SignIn V1

--- WebApplication1/userlogin.aspx.cs
+++ WebApplication1/userlogin.aspx.cs
@@ -11,7 +11,7 @@ namespace WebApplication1
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            Response.Write("<script>alert('Button Clicked');</script>");
         }
     }
 }
