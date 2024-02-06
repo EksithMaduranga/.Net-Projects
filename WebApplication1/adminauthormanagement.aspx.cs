3d02ab9e9c109ad8e117777432a70fae90dd7002
EksithMaduranga <eksithmadu9@gmail.com>
2024-02-06 12:37:26 +0530

    FinalizedToCheck

--- WebApplication1/adminauthormanagement.aspx.cs
+++ WebApplication1/adminauthormanagement.aspx.cs
@@ -10,61 +10,69 @@ using System.Web.UI.WebControls;
 
 namespace WebApplication1
 {
-    public partial class adminauthormanagement : System.Web.UI.Page
+    public partial class adminbookissuing : System.Web.UI.Page
     {
         string strcon = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
         protected void Page_Load(object sender, EventArgs e)
         {
             GridView1.DataBind();
         }
-        // add button click
+
+        // issue book
         protected void Button2_Click(object sender, EventArgs e)
         {
-            if (checkIfAuthorExists())
+            if (checkIfBookExist() && checkIfMemberExist())
             {
-                Response.Write("<script>alert('Author with this ID already Exist. You cannot add another Author with the same Author ID');</script>");
-            }
-            else
-            {
-                addNewAuthor();
-            }
-        }
-        // update button click
-        protected void Button3_Click(object sender, EventArgs e)
-        {
-            if (checkIfAuthorExists())
-            {
-                updateAuthor();
+
+                if (checkIfIssueEntryExist())
+                {
+                    Response.Write("<script>alert('This Member already has this book');</script>");
+                }
+                else
+                {
+                    issueBook();
+                }
 
             }
             else
             {
-                Response.Write("<script>alert('Author does not exist');</script>");
+                Response.Write("<script>alert('Wrong Book ID or Member ID');</script>");
             }
         }
-        // delete button click
+        // return book
         protected void Button4_Click(object sender, EventArgs e)
         {
-            if (checkIfAuthorExists())
+            if (checkIfBookExist() && checkIfMemberExist())
             {
-                deleteAuthor();
+
+                if (checkIfIssueEntryExist())
+                {
+                    returnBook();
+                }
+                else
+                {
+                    Response.Write("<script>alert('This Entry does not exist');</script>");
+                }
 
             }
             else
             {
-                Response.Write("<script>alert('Author does not exist');</script>");
+                Response.Write("<script>alert('Wrong Book ID or Member ID');</script>");
             }
         }
-        // GO button click
+
+        // go button click event
         protected void Button1_Click(object sender, EventArgs e)
         {
-            getAuthorByID();
+            getNames();
         }
+
 
 
 
         // user defined function
-        void getAuthorByID()
+
+        void returnBook()
         {
             try
             {
@@ -74,31 +82,36 @@ namespace WebApplication1
                     con.Open();
                 }
 
-                SqlCommand cmd = new SqlCommand("SELECT * from author_master_tbl where author_id='" + TextBox1.Text.Trim() + "';", con);
-                SqlDataAdapter da = new SqlDataAdapter(cmd);
-                DataTable dt = new DataTable();
-                da.Fill(dt);
 
-                if (dt.Rows.Count >= 1)
+                SqlCommand cmd = new SqlCommand("Delete from book_issue_table WHERE book_id='" + TextBox1.Text.Trim() + "' AND member_id='" + TextBox2.Text.Trim() + "'", con);
+                int result = cmd.ExecuteNonQuery();
+
+                if (result > 0)
                 {
-                    TextBox2.Text = dt.Rows[0][1].ToString();
+
+                    cmd = new SqlCommand("update book_master_tbl set current_stock = current_stock+1 WHERE book_id='" + TextBox1.Text.Trim() + "'", con);
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+
+                    Response.Write("<script>alert('Book Returned Successfully');</script>");
+                    GridView1.DataBind();
+
+                    con.Close();
+
                 }
                 else
                 {
-                    Response.Write("<script>alert('Invalid Author ID');</script>");
+                    Response.Write("<script>alert('Error - Invalid details');</script>");
                 }
-
 
             }
             catch (Exception ex)
             {
                 Response.Write("<script>alert('" + ex.Message + "');</script>");
-
             }
         }
 
-
-        void deleteAuthor()
+        void issueBook()
         {
             try
             {
@@ -108,39 +121,24 @@ namespace WebApplication1
                     con.Open();
                 }
 
-                SqlCommand cmd = new SqlCommand("DELETE from author_master_tbl WHERE author_id='" + TextBox1.Text.Trim() + "'", con);
+                SqlCommand cmd = new SqlCommand("INSERT INTO book_issue_table(member_id,member_name,book_id,book_name,issue_date,due_date) values(@member_id,@member_name,@book_id,@book_name,@issue_date,@due_date)", con);
+
+                cmd.Parameters.AddWithValue("@member_id", TextBox2.Text.Trim());
+                cmd.Parameters.AddWithValue("@member_name", TextBox3.Text.Trim());
+                cmd.Parameters.AddWithValue("@book_id", TextBox1.Text.Trim());
+                cmd.Parameters.AddWithValue("@book_name", TextBox4.Text.Trim());
+                cmd.Parameters.AddWithValue("@issue_date", TextBox5.Text.Trim());
+                cmd.Parameters.AddWithValue("@due_date", TextBox6.Text.Trim());
 
                 cmd.ExecuteNonQuery();
-                con.Close();
-                Response.Write("<script>alert('Author Deleted Successfully');</script>");
-                clearForm();
-                GridView1.DataBind();
 
-            }
-            catch (Exception ex)
-            {
-                Response.Write("<script>alert('" + ex.Message + "');</script>");
-            }
-        }
-
-        void updateAuthor()
-        {
-            try
-            {
-                SqlConnection con = new SqlConnection(strcon);
-                if (con.State == ConnectionState.Closed)
-                {
-                    con.Open();
-                }
-
-                SqlCommand cmd = new SqlCommand("UPDATE author_master_tbl SET author_name=@author_name WHERE author_id='" + TextBox1.Text.Trim() + "'", con);
-
-                cmd.Parameters.AddWithValue("@author_name", TextBox2.Text.Trim());
+                cmd = new SqlCommand("update  book_master_tbl set current_stock = current_stock-1 WHERE book_id='" + TextBox1.Text.Trim() + "'", con);
 
                 cmd.ExecuteNonQuery();
+
                 con.Close();
-                Response.Write("<script>alert('Author Updated Successfully');</script>");
-                clearForm();
+                Response.Write("<script>alert('Book Issued Successfully');</script>");
+
                 GridView1.DataBind();
             }
             catch (Exception ex)
@@ -149,8 +147,7 @@ namespace WebApplication1
             }
         }
 
-
-        void addNewAuthor()
+        bool checkIfBookExist()
         {
             try
             {
@@ -159,41 +156,10 @@ namespace WebApplication1
                 {
                     con.Open();
                 }
-
-                SqlCommand cmd = new SqlCommand("INSERT INTO author_master_tbl(author_id,author_name) values(@author_id,@author_name)", con);
-
-                cmd.Parameters.AddWithValue("@author_id", TextBox1.Text.Trim());
-                cmd.Parameters.AddWithValue("@author_name", TextBox2.Text.Trim());
-
-                cmd.ExecuteNonQuery();
-                con.Close();
-                Response.Write("<script>alert('Author added Successfully');</script>");
-                clearForm();
-                GridView1.DataBind();
-            }
-            catch (Exception ex)
-            {
-                Response.Write("<script>alert('" + ex.Message + "');</script>");
-            }
-        }
-
-
-
-        bool checkIfAuthorExists()
-        {
-            try
-            {
-                SqlConnection con = new SqlConnection(strcon);
-                if (con.State == ConnectionState.Closed)
-                {
-                    con.Open();
-                }
-
-                SqlCommand cmd = new SqlCommand("SELECT * from author_master_tbl where author_id='" + TextBox1.Text.Trim() + "';", con);
+                SqlCommand cmd = new SqlCommand("select * from book_master_tbl WHERE book_id='" + TextBox1.Text.Trim() + "' AND current_stock >0", con);
                 SqlDataAdapter da = new SqlDataAdapter(cmd);
                 DataTable dt = new DataTable();
                 da.Fill(dt);
-
                 if (dt.Rows.Count >= 1)
                 {
                     return true;
@@ -202,20 +168,136 @@ namespace WebApplication1
                 {
                     return false;
                 }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+
+        bool checkIfMemberExist()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(strcon);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+                SqlCommand cmd = new SqlCommand("select full_name from member_master_table WHERE member_id='" + TextBox2.Text.Trim() + "'", con);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                if (dt.Rows.Count >= 1)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+
+        bool checkIfIssueEntryExist()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(strcon);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+                SqlCommand cmd = new SqlCommand("select * from book_issue_table WHERE member_id='" + TextBox2.Text.Trim() + "' AND book_id='" + TextBox1.Text.Trim() + "'", con);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                if (dt.Rows.Count >= 1)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+
+
+
+        void getNames()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(strcon);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+                SqlCommand cmd = new SqlCommand("select book_name from book_master_tbl WHERE book_id='" + TextBox1.Text.Trim() + "'", con);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                if (dt.Rows.Count >= 1)
+                {
+                    TextBox4.Text = dt.Rows[0]["book_name"].ToString();
+                }
+                else
+                {
+                    Response.Write("<script>alert('Wrong Book ID');</script>");
+                }
+
+                cmd = new SqlCommand("select full_name from member_master_table WHERE member_id='" + TextBox2.Text.Trim() + "'", con);
+                da = new SqlDataAdapter(cmd);
+                dt = new DataTable();
+                da.Fill(dt);
+                if (dt.Rows.Count >= 1)
+                {
+                    TextBox3.Text = dt.Rows[0]["full_name"].ToString();
+                }
+                else
+                {
+                    Response.Write("<script>alert('Wrong User ID');</script>");
+                }
 
 
             }
             catch (Exception ex)
             {
-                Response.Write("<script>alert('" + ex.Message + "');</script>");
-                return false;
+
             }
         }
 
-        void clearForm()
+        protected void GridView1_RowDataBound(object sender, GridViewRowEventArgs e)
         {
-            TextBox1.Text = "";
-            TextBox2.Text = "";
+            try
+            {
+                if (e.Row.RowType == DataControlRowType.DataRow)
+                {
+                    //Check your condition here
+                    DateTime dt = Convert.ToDateTime(e.Row.Cells[5].Text);
+                    DateTime today = DateTime.Today;
+                    if (today > dt)
+                    {
+                        e.Row.BackColor = System.Drawing.Color.PaleVioletRed;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
         }
     }
 }
--- WebApplication1/userprofile.aspx.designer.cs
+++ WebApplication1/userprofile.aspx.designer.cs
@@ -15,6 +15,15 @@ namespace WebApplication1
     {
 
         /// <summary>
+        /// Label1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+
+        /// <summary>
         /// TextBox1 control.
         /// </summary>
         /// <remarks>
@@ -22,15 +31,6 @@ namespace WebApplication1
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox TextBox1;
-
-        /// <summary>
-        /// Button1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button1;
 
         /// <summary>
         /// TextBox2 control.
@@ -42,31 +42,103 @@ namespace WebApplication1
         protected global::System.Web.UI.WebControls.TextBox TextBox2;
 
         /// <summary>
-        /// Button2 control.
+        /// TextBox3 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button2;
+        protected global::System.Web.UI.WebControls.TextBox TextBox3;
 
         /// <summary>
-        /// Button3 control.
+        /// TextBox4 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button3;
+        protected global::System.Web.UI.WebControls.TextBox TextBox4;
 
         /// <summary>
-        /// Button4 control.
+        /// DropDownList1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button4;
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList1;
+
+        /// <summary>
+        /// TextBox6 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox6;
+
+        /// <summary>
+        /// TextBox7 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox7;
+
+        /// <summary>
+        /// TextBox5 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox5;
+
+        /// <summary>
+        /// TextBox8 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox8;
+
+        /// <summary>
+        /// TextBox9 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox9;
+
+        /// <summary>
+        /// TextBox10 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox10;
+
+        /// <summary>
+        /// Button1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+
+        /// <summary>
+        /// Label2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
 
         /// <summary>
         /// GridView1 control.
